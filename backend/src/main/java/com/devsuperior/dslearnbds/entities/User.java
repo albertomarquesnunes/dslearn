7c78a9fc9902272f70b227bc684501ec9246d0a4
Alberto Marques Nunes <alberto.nunes@primesolutions.com.br>
2022-12-27 21:22:13 -0300

    Final fixes

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/User.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/User.java
@@ -41,13 +41,13 @@ public class User implements Serializable{
 	
 	public User() {}
 
-	public User(Long id, String name, String email, String password, Set<Role> roles) {
+	public User(Long id, String name, String email, String password) {
 		
 		this.id = id;
 		this.name = name;
 		this.email = email;
 		this.password = password;
-		this.roles = roles;
+		
 	}
 
 	public Long getId() {
@@ -86,8 +86,9 @@ public class User implements Serializable{
 		return roles;
 	}
 
-	public void setRoles(Set<Role> roles) {
-		this.roles = roles;
+
+	public List<Notification> getNotifications() {
+		return notifications;
 	}
 
 	@Override
